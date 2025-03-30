351d8f4e0ebc928b3bbc0194bcf7999e1200c6ee
Roberto Albergo <ralbergo7@gmail.com>
2025-03-30 16:54:16 +0200

    footer

---
+++ HTML/Script/index.js
@@ -0,0 +1,6 @@
+const triumvire = function () {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdn.jsdelivr.net/gh/andreacirilloac/tv-remote@main/HTML/Script/tv-remote.js";
+  document.head.appendChild(script);
+};
